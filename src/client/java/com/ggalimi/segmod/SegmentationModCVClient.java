b619003527414b9dbee1730eacaf8bdebe26e9ef
griffing52 <emailgriffinnow@gmail.com>
2025-11-15 17:26:39 -0800

    Fabric mod init

---
+++ src/client/java/com/ggalimi/segmod/SegmentationModCVClient.java
@@ -0,0 +1,10 @@
+package com.ggalimi.segmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SegmentationModCVClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
